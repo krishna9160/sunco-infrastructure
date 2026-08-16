69e9d3717a7b03f78008c1407abb3eb2483d978d
krishna9160 <119475690+krishna9160@users.noreply.github.com>
2026-08-16 18:03:38 +0530

    Update backend.tf

--- sunco-infrastructure-terraform/terraform/backend.tf
+++ sunco-infrastructure-terraform/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sunco-terraform-state-file"
+    bucket = "sunco-terraform-state-filestorage"
     key    = "sunco/terraform.tfstate"
     region = "ap-southeast-1"
   }
